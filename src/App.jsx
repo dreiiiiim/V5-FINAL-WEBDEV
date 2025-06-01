566172920ad89ad3b14898e4bded0e9a8d460821
svixco <andrei.montaniel.cics@ust.edu.ph>
2025-06-01 13:10:35 +0800

    /#/aut/callback change

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import MonthlyCalendar from './Components/MonthlyCalendar';
-import Welcomepage from './Components/Welcomepage';
-import AuthRedirect from './Components/AuthRedirect'; // ✅ Don't forget this!
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import MonthlyCalendar from "./Components/MonthlyCalendar";
+import Welcomepage from "./Components/Welcomepage";
+import AuthRedirect from "./Components/AuthRedirect"; // ✅ Don't forget this!
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className='h-screen w-screen'>
+    <div className="h-screen w-screen">
       <Routes>
-        <Route path='/' element={<Welcomepage />} />
+        <Route path="/" element={<Welcomepage />} />
         {/* <Route path='/MonthlyCalendar' element={<MonthlyCalendar />} /> */}
-        <Route path="/auth/callback" element={<AuthRedirect />} />
-<Route path="/MonthlyCalendar" element={<MonthlyCalendar />} />
+        <Route path="/#/auth/callback" element={<AuthRedirect />} />
+        <Route path="/MonthlyCalendar" element={<MonthlyCalendar />} />
       </Routes>
-
-  
     </div>
   );
 };
