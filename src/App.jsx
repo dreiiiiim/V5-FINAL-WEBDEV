38f99758778e21dd3f50f90b727e71db46c5443c
svixco <andrei.montaniel.cics@ust.edu.ph>
2025-06-01 14:46:15 +0800

    hasrouter appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import React from "react";
+// import React from "react";
+// import MonthlyCalendar from "./Components/MonthlyCalendar";
+// import Welcomepage from "./Components/Welcomepage";
+// import AuthRedirect from "./Components/AuthRedirect";
+// import {  Routes, Route } from "react-router-dom";
+
+// const App = () => {
+  
+//   return (
+    
+//       <div className="h-screen w-screen">
+//       <Routes>
+//         <Route path="/" element={<Welcomepage />} />
+//         <Route path="/MonthlyCalendar" element={<MonthlyCalendar />} />
+//         <Route path="/auth/callback" element={<AuthRedirect />} />
+//       </Routes>
+//     </div>
+
+    
+//   );
+// };
+
+// export default App;
+
+import { HashRouter, Routes, Route } from "react-router-dom";
 import MonthlyCalendar from "./Components/MonthlyCalendar";
 import Welcomepage from "./Components/Welcomepage";
 import AuthRedirect from "./Components/AuthRedirect";
-import {  Routes, Route } from "react-router-dom";
 
-const App = () => {
-  
-  return (
-    
-      <div className="h-screen w-screen">
-      <Routes>
-        <Route path="/" element={<Welcomepage />} />
-        <Route path="/MonthlyCalendar" element={<MonthlyCalendar />} />
-        <Route path="/auth/callback" element={<AuthRedirect />} />
-      </Routes>
-    </div>
-
-    
-  );
-};
+const App = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<Welcomepage />} />
+      <Route path="/MonthlyCalendar" element={<MonthlyCalendar />} />
+      <Route path="/auth/callback" element={<AuthRedirect />} />
+    </Routes>
+  </HashRouter>
+);
 
 export default App;
