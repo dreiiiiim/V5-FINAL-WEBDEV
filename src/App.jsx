a5bd8f860533dd58560e6966b6f0560c22cd707e
svixco <andrei.montaniel.cics@ust.edu.ph>
2025-06-01 16:37:15 +0800

    hash removed

--- src/App.jsx
+++ src/App.jsx
@@ -42,13 +42,16 @@
 // );
 
 // export default App;
-import { HashRouter, Routes, Route } from "react-router-dom";
+// 
+
+
+import {  Routes, Route } from "react-router-dom";
 import MonthlyCalendar from "./Components/MonthlyCalendar";
 import Welcomepage from "./Components/Welcomepage";
 import AuthRedirect from "./Components/AuthRedirect";
 
 const App = () => (
-  <HashRouter>
+
     <div className="h-screen w-screen">
       <Routes>
         <Route path="/" element={<Welcomepage />} />
@@ -56,7 +59,7 @@ const App = () => (
         <Route path="/auth/callback" element={<AuthRedirect />} />
       </Routes>
     </div>
-  </HashRouter>
+
 );
 
 export default App;
