7d0293d8430f2b5c2c6684cdb718d0ce6be354ad
svixco <andrei.montaniel.cics@ust.edu.ph>
2025-06-01 11:52:56 +0800

    Switched from BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' // Changed from BrowserRouter
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
